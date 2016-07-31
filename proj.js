cc585dda50b95c78d5511d058bd7f2e779a35446
RegB <beason.reginald@gmail.com>
2016-07-31 12:50:10 -0700

    images rename

--- proj.js
+++ proj.js
@@ -1,7 +1,29 @@
 var canvas = document.getElementById('myCanvas');
-var context = canvas.getContext("2d");
+var ctx = canvas.getContext("2d");
 var img = new Image();
-img.src = "images\gifs-animados-zombies-7300787.gif";
 window.addEventListener('load', function () {
-    context.drawImage(img, 0, 0);
+    img.src = 'BrainEaterRegEmma\images\cat.gif';
+    ctx.drawImage(img, 0, 0);
 });
+var Map = (function () {
+    function Map(theMap, difficulty) {
+        this.theMap = theMap;
+        this.difficulty = difficulty;
+    }
+    return Map;
+}());
+var aMap = [
+    new Map("map1", "zombieSpd"),
+    new Map("map2", "zombieSpd"),
+    new Map("map3", "zombieSpd"),
+    new Map("map4", "zombieSpd"),
+    new Map("map5", "zombieSpd"),
+];
+var bMap = [];
+var currentMap = [];
+var canvasDis = document.getElementsByTagName('div');
+for (var i = 0; i < 3; i++) {
+    var mapGen = Math.floor(Math.random() * aMap.length);
+    bMap.push(aMap[mapGen]);
+    aMap.splice(mapGen, 1);
+}
--- proj.ts
+++ proj.ts
@@ -1,12 +1,53 @@
 // create canvas
 let canvas = <HTMLCanvasElement>document.getElementById('myCanvas');
-let context = canvas.getContext("2d");
+let ctx = canvas.getContext("2d");
 
-// get zombie image
+// get cat image
 let img = new Image();
-img.src = "images\gifs-animados-zombies-7300787.gif";
 
-// when all images loaded, start drawing
+//when all images loaded, start drawing
 window.addEventListener('load', () => {
-    context.drawImage(img, 0, 0);
+
+    img.src = 'BrainEaterRegEmma\images\cat.gif';
+    ctx.drawImage(img, 0, 0);
+
 });
+
+//Map generator - it will be three difficulty settings, easy, med, & hard.
+class Map {
+  constructor (public theMap:string, public difficulty:string){
+      }
+}
+ let aMap = [
+    new Map(`map1`, `zombieSpd`),
+    new Map(`map2`, `zombieSpd`),
+    new Map(`map3`, `zombieSpd`),
+    new Map(`map4`, `zombieSpd`),
+    new Map(`map5`, `zombieSpd`),
+];
+// console.log(aQuestion);
+let bMap = []; //the 3 maps we will cycle through
+let currentMap = []; // this empty array that will only display the actually map you are on.
+
+// gets the div element where we will place the map on the page
+let canvasDis = document.getElementsByTagName('div');
+
+
+for (let i=0; i < 3; i++) {
+  let mapGen = Math.floor(Math.random()*aMap.length);
+  bMap.push(aMap[mapGen]);
+  aMap.splice(mapGen, 1);
+
+}
+
+// function displayQ() {
+// for (let i=0; i < bMap.length; i++){
+//         currentMap.push(bMap[i].question + typeChecker(bMap[i]));
+//         console.log(currentMap[i]);
+//     }
+// canvasDis[0].innerHTML = currentMap[0];
+//
+// }
+
+
+// displayQ();
